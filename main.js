6ad5becd9e505ba0a8146bf066b1c8b8ebf484e8
Dan Miller <dmm4613@gmail.com>
2020-04-28 09:58:37 -0400

    added repeat check to make sure key is not registered as repeating.

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
+
+
 window.addEventListener('keydown', function(e) {
+    if (e.repeat) {return}
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
     if(!audio) return;
     audio.currentTime=0;
     audio.play()
     key.classList.add('playing');
+     
 });
+
 
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
     this.classList.remove('playing');
+    keydown = false;
 }
 
 const keys = document.querySelectorAll('.key');
